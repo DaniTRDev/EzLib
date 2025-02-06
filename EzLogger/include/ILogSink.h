1e9821e9eef1e1f8ded790285129889ab90a3065
OsiKaSuKe <43011685+DaniTRDev@users.noreply.github.com>
2025-02-06 12:52:47 +0100

    Added ILogSink.h after a weird error on Git end's deleted it.

---
+++ EzLogger/include/ILogSink.h
@@ -0,0 +1,30 @@
+#ifndef EZLOGGER_ILOGSINK_H
+#define EZLOGGER_ILOGSINK_H
+
+#include "../../Common/Common.h"
+
+/**
+ * This class represents an interface for a log sink.
+ */
+class ILogSink
+{
+  public:
+    
+    /**
+     * Destroys the object.
+     */
+    virtual ~ILogSink() = default;
+    
+    /**
+     * Pushes a log to the internal logger and returns true if succeeded.
+     * @param msg
+     * @return bool
+     */
+    virtual bool pushLog(const class LogMessage &msg) = 0;
+    
+  protected:
+    class ILogger *m_logger{}; // Used by this sink where to send the log to.
+    class LogSegment m_prefix{""};
+};
+
+#endif // EZLOGGER_ILOGSINK_H
