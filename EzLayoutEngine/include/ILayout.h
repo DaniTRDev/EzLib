88d075621368fef789fa2604289cf4837445675d
OsiKaSuKe <43011685+DaniTRDev@users.noreply.github.com>
2025-03-14 00:20:52 +0100

    Created the interface for a basic layout element. TODO: Finish.

---
+++ EzLayoutEngine/include/ILayout.h
@@ -0,0 +1,16 @@
+#ifndef EZLIB_ILAYOUT_H
+#define EZLIB_ILAYOUT_H
+
+#include "EzLibCommon.h"
+
+/**
+ * This interface provides a set of operations that should be defined for a layout element.
+ */
+class ILayout
+{
+  public:
+  
+  private:
+};
+
+#endif // EZLIB_ILAYOUT_H
