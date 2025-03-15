5d583d5e32fe8759aadabf44263c8b4005191850
OsiKaSuKe <43011685+DaniTRDev@users.noreply.github.com>
2025-03-16 00:12:43 +0100

    Made clipper work with physical size and clipped size.

--- EzLayoutEngine/src/Clipping/Clipper.cpp
+++ EzLayoutEngine/src/Clipping/Clipper.cpp
@@ -1,6 +1,11 @@
 #include "Clipping/Clipper.h"
 
-Clipper::Clipper(Vec2 startingPos, Vec2 startingSize) : m_pos(startingPos), m_size(startingSize)
+Clipper::Clipper() : m_physicalSize(), m_pos(), m_size()
+{
+}
+
+Clipper::Clipper(Vec2 physicalSize, Vec2 startingPos, Vec2 startingSize)
+    : m_physicalSize(physicalSize), m_pos(startingPos), m_size(startingSize)
 {
 }
 
@@ -12,21 +17,40 @@ bool Clipper::contains(const Vec2 &pos) const
      */
     uint32_t thisX = m_pos.getX();
     uint32_t thisY = m_pos.getY();
-    
+
     uint32_t otherX = pos.getX();
     uint32_t otherY = pos.getY();
-    
+
     return (otherX >= thisX) && (otherY >= thisY) && (otherX <= (thisX + m_size.getX())) &&
            (otherY <= (thisY + m_size.getY()));
 }
 
+bool Clipper::containsPhysically(const Vec2 &pos) const
+{
+    /* Save variables that are going to be used more than once. Theoretically compiler would optimize this,
+     * but we can't assume it. Since this will be called VERY, VERY, VERY OFTEN anything that can be made faster is
+     * used.
+     */
+    uint32_t thisX = m_pos.getX();
+    uint32_t thisY = m_pos.getY();
+    
+    uint32_t otherX = pos.getX();
+    uint32_t otherY = pos.getY();
+
+    return (otherX >= thisX) && (otherY >= thisY) && (otherX <= (thisX + m_physicalSize.getX())) &&
+           (otherY <= (thisY + m_physicalSize.getY()));
+}
+
 bool Clipper::resize(const Vec2 &newSize)
 {
-    if (newSize.getX() < m_pos.getX() || newSize.getY() < m_size.getY())
-        return false;
-
     m_size = newSize;
-    return true;
+    return m_size <= m_physicalSize;
+}
+
+bool Clipper::resizePhysically(const Vec2 &newSize)
+{
+    m_physicalSize = newSize;
+    return m_size <= m_physicalSize;
 }
 
 const Vec2 &Clipper::getPos() const
