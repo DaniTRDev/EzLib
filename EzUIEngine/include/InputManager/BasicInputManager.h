ed8cb8e6b7fe7887e4bb59987741753fd31daf11
OsiKaSuKe <43011685+DaniTRDev@users.noreply.github.com>
2025-03-16 21:33:20 +0100

    Created a basic input manager class that will handle everything related to inputs.

---
+++ EzUIEngine/include/InputManager/BasicInputManager.h
@@ -0,0 +1,11 @@
+#ifndef EZLIB_BASICINPUTMANAGER_H
+#define EZLIB_BASICINPUTMANAGER_H
+
+#include "IInputManager.h"
+
+class BasicInputManager : public IInputManager
+{
+
+};
+
+#endif // EZLIB_BASICINPUTMANAGER_H
---
+++ EzUIEngine/src/InputManager/BasicInputManager.cpp
@@ -0,0 +1,1 @@
+#include "InputManager/BasicInputManager.h"
