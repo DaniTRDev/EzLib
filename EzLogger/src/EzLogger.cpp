f680fda8ecab2b1dbcdb3875de94dff67b83a438
OsiKaSuKe <43011685+DaniTRDev@users.noreply.github.com>
2025-08-20 20:19:33 +0200

    Fixed a bug in createSinkLogger that tried to write into out buffer when it was not still opened.

--- EzLogger/src/EzLogger.cpp
+++ EzLogger/src/EzLogger.cpp
@@ -4,13 +4,19 @@ namespace EzLogger
 {
 	std::unique_ptr<SyncLogger> createSinkLogger(std::string_view consoleTittle, std::filesystem::path filePath)
 	{
-		std::unique_ptr<SyncLogger> sync = std::make_unique<SyncLogger>();
-		sync->addBuffer(std::make_unique<ConsoleOutLogBuffer>(consoleTittle));
+        std::unique_ptr<ConsoleOutLogBuffer> consoleBuff = std::make_unique<ConsoleOutLogBuffer>(consoleTittle);
+        std::unique_ptr<FileOutLogBuffer> fileBuff = std::make_unique<FileOutLogBuffer>(filePath);
+        std::unique_ptr<SyncLogger> sync = std::make_unique<SyncLogger>();
+        
+        if (!consoleBuff->open())
+        {
+            throw std::runtime_error("Could not open console buffer");
+        }
+        
+		sync->addBuffer(std::move(consoleBuff));
         
         if (filePath != "")
         {
-            std::unique_ptr<FileOutLogBuffer> fileBuff = std::make_unique<FileOutLogBuffer>(filePath);
-            
             if (!fileBuff->open())
                 sync->pushLog(LogMessage("Could not open out log file!"));
             else
