71ebdb47f545843d4679d226914aad1a5912c388
OsiKaSuKe <43011685+DaniTRDev@users.noreply.github.com>
2025-08-25 20:44:58 +0200

    Fixed a bug within EzCMK_AddVendor's call to FetchContent_Declare.

--- EzCMK/Vendor.cmake
+++ EzCMK/Vendor.cmake
@@ -5,7 +5,7 @@ function(EzCMK_AddVendor LIB_GIT_REPO LIB_GIT_TAG TARGET_NAME)
     message(STATUS "Importing ${GIT_REPO}:${GIT_TAG} with name ${TARGET_NAME}")
     FetchContent_Declare(
             ${TARGET_NAME}
-            GIT_URL LIB_GIT_REPO
+            GIT_REPOSITORY LIB_GIT_REPO
             GIT_TAG LIB_GIT_TAG
     )
     FetchContent_MakeAvailable(${TARGET_NAME})
