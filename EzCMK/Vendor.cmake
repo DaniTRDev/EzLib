7f129b85f66db50e759b14f8efaf2cdb2d227e92
OsiKaSuKe <43011685+DaniTRDev@users.noreply.github.com>
2025-11-02 20:59:26 +0100

    Fixed an error with fetched project source directory not being exported at all.

--- EzCMK/Vendor.cmake
+++ EzCMK/Vendor.cmake
@@ -2,11 +2,13 @@
 
 
 function(EzCMK_AddVendor LIB_GIT_REPO LIB_GIT_TAG TARGET_NAME)
-    message(STATUS "Importing ${GIT_REPO}:${GIT_TAG} with name ${TARGET_NAME}")
+    message(STATUS "Importing ${LIB_GIT_REPO}:${LIB_GIT_TAG} with name ${TARGET_NAME}")
     FetchContent_Declare(
             ${TARGET_NAME}
             GIT_REPOSITORY ${LIB_GIT_REPO}
             GIT_TAG ${LIB_GIT_TAG}
     )
     FetchContent_MakeAvailable(${TARGET_NAME})
+    set(GIT_PRJ_DIR "${TARGET_NAME}_SOURCE_DIR")
+    set(${GIT_PRJ_DIR} "${${GIT_PRJ_DIR}}" PARENT_SCOPE)
 endfunction()
