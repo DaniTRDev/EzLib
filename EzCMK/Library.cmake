2a5967f043fe437059d97facbf9cd3b8e27817cc
DaniTR <43011685+DaniTRDev@users.noreply.github.com>
2026-08-16 16:46:54 +0200

    Enhance EzCMK_ConfigureLib for module file support
    Refactor EzCMK_ConfigureLib to handle module files and improve target source management.

--- EzCMK/Library.cmake
+++ EzCMK/Library.cmake
@@ -34,16 +34,15 @@ endfunction()
 function(EzCMK_ConfigureLib)
     set(options)  # no boolean switches
     set(oneValueArgs NAME PRECOMPILED_HEADER BUILD_TYPE FORCE_CREATION)
-    set(multiValueArgs FILE_LIST LINKED_LIBRARIES INCLUDED_DIRS DEFINITIONS)
+    set(multiValueArgs FILE_LIST MODULE_FILES LINKED_LIBRARIES INCLUDED_DIRS DEFINITIONS)
     cmake_parse_arguments("ARG" "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    if (DEFINED ARG_FORCE_CREATION)
-        if (ARG_FORCE_CREATION)
-            EzCMK_CreateTarget(NAME ${ARG_NAME}
-                    TARGET_DIR_PATH ${CMAKE_CURRENT_SOURCE_DIR}
-            )
-        endif ()
+    if (DEFINED ARG_FORCE_CREATION AND ARG_FORCE_CREATION)
+        EzCMK_CreateTarget(NAME ${ARG_NAME}
+                TARGET_DIR_PATH ${CMAKE_CURRENT_SOURCE_DIR}
+        )
     endif ()
+
     EzCMK_GetLibraryTargetName(${ARG_NAME} TARGET_NAME)
     EzCMK_GetLibraryBuildType(OUT_VAR TARGET_BUILD_TYPE
             FORCE_VALUE ${ARG_BUILD_TYPE}
@@ -51,28 +50,43 @@ function(EzCMK_ConfigureLib)
 
     message(STATUS "Configuring LIBRARY ${ARG_NAME} (target: ${TARGET_NAME}) (build type: ${TARGET_BUILD_TYPE})")
 
-    # Define an alias
-    add_library(${ARG_NAME} ${TARGET_BUILD_TYPE} ${ARG_FILE_LIST})
-    add_library(${TARGET_NAME} ALIAS ${ARG_NAME}) # Use our library
+    # 1. Create target without hardcoding sources directly into add_library
+    add_library(${ARG_NAME} ${TARGET_BUILD_TYPE})
+    add_library(${TARGET_NAME} ALIAS ${ARG_NAME})
 
     set_target_properties(${ARG_NAME} PROPERTIES OUTPUT_NAME "${ARG_NAME}")
 
-    if (DEFINED ARG_LINKED_LIBRARIES)
+    # 2. Add regular implementation/source files (.cpp)
+    if (ARG_FILE_LIST)
+        target_sources(${ARG_NAME} PRIVATE ${ARG_FILE_LIST})
+    endif ()
+
+    # 3. Add C++20 Module Interfaces and Partitions (.cppm)
+    if (ARG_MODULE_FILES)
+        target_sources(${ARG_NAME}
+            PUBLIC
+                FILE_SET CXX_MODULES FILES ${ARG_MODULE_FILES}
+        )
+        message(VERBOSE "C++20 Module interfaces: ${ARG_MODULE_FILES}")
+    endif ()
+
+    if (ARG_LINKED_LIBRARIES)
         target_link_libraries(${ARG_NAME} PUBLIC ${ARG_LINKED_LIBRARIES})
         message(VERBOSE "Library dependencies: ${ARG_LINKED_LIBRARIES}")
     endif ()
 
-    if (DEFINED ARG_INCLUDED_DIRS)
+    if (ARG_INCLUDED_DIRS)
         target_include_directories(${ARG_NAME} PUBLIC ${ARG_INCLUDED_DIRS})
         message(VERBOSE "Included directories: ${ARG_INCLUDED_DIRS}")
     endif ()
 
-    if (DEFINED ARG_DEFINITIONS)
+    if (ARG_DEFINITIONS)
         message(VERBOSE "Definitions: ${ARG_DEFINITIONS}")
         target_compile_definitions(${ARG_NAME} PUBLIC ${ARG_DEFINITIONS})
     endif ()
 
-    if (DEFINED ARG_PRECOMPILED_HEADER)
+    # 4. Safe check so passing PRECOMPILED_HEADER "" does not trigger an error
+    if (ARG_PRECOMPILED_HEADER AND NOT ARG_PRECOMPILED_HEADER STREQUAL "")
         message(VERBOSE "Precompiled header: ${ARG_PRECOMPILED_HEADER}")
         target_precompile_headers(${ARG_NAME} PUBLIC ${ARG_PRECOMPILED_HEADER})
     endif ()
